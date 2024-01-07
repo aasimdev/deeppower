68efb0c8dbc0917e0b0669ebb371a1f4a374786f
aasimdev <aasimdev69@gmail.com>
2024-01-07 23:00:54 +0500

    updated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,15 @@ $(function () {
     var pathname = window.location.pathname;
     $(window).scroll(function () {
         if ($(window).scrollTop() >= 50) {
+            console.log(pathname);
             $('.header-top').addClass('fixed');
-            if (pathname === "/" && pathname === "index.php" && pathname === "" && pathname === "investors.php" && pathname === "about-us") {
+            if (pathname === "/index.php" || pathname === "/investors.php" || pathname === "/about-us.php" || pathname === "/" || pathname === "") {
                 $('.navbar-brand').html('<img src="./assets/img/dark-logo.svg" alt="logo"/>');
             }
         }
         else {
             $('.header-top').removeClass('fixed');
-            if (pathname === "/" && pathname === "index.php" && pathname === "" && pathname === "investors.php" && pathname === "about-us") {
+            if (pathname === "/index.php" || pathname === "/investors.php" || pathname === "/about-us.php" || pathname === "/" || pathname === "") {
                 $('.navbar-brand').html('<img src="./assets/img/light-logo.svg" alt="logo"/>');
             }
         }
